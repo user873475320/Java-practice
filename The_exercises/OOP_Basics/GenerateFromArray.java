66c0d4672aa7fdad959ac160e76a076decb1e276
none <user873475320@gmail.com>
2022-09-03 23:05:29 +0500

    03.09.2022

--- The_exercises/OOP_Basics/GenerateFromArray.java
+++ The_exercises/OOP_Basics/GenerateFromArray.java
@@ -1,4 +1,39 @@
 package The_exercises.OOP_Basics;
 
+import java.util.Random;
+
 public class GenerateFromArray {
+    private final int[] values;
+    private final int[] weights;
+
+    public GenerateFromArray(int[] values, int[] weights) {
+        this.values = values;
+        this.weights = weights;
+    }
+
+    // My realization
+    public int getRandomFromArr() {
+        Random rand = new Random();
+        for (int i = 0; i < weights.length; i++) {
+            for (int j = 0; j < weights[i]; j++) {
+                if (values[rand.nextInt(values.length)] == values[i]) return values[i];
+            }
+        }
+        return values[rand.nextInt(values.length)];
+    }
+
+    public int getRandom() {
+        Random rand = new Random();
+
+        int sum = 0;
+        for (var weight : weights) sum += weight;
+        int[] newArr = new int[sum];
+
+        int index = 0;
+        for (int i = 0; i < weights.length; i++) {
+            for (int j = 0; j < weights[i]; j++) newArr[index++] = values[i];
+        }
+
+        return values[rand.nextInt(sum)];
+    }
 }
---
+++ The_exercises/OOP_Basics/MyLinkedList.java
@@ -0,0 +1,74 @@
+package The_exercises.OOP_Basics;
+
+public class MyLinkedList<T> {
+    transient private Node<T> first = null;
+    transient private Node<T> last = null;
+    transient private int size = 0;
+
+    public void add(T obj) {
+        if (first == null) {
+            first = new Node<>(null, obj, null);
+            last = first;
+            size++;
+        }
+        Node<T> l = last;
+        last = new Node<>(l, obj, null);
+        l.next = last;
+        size++;
+    }
+
+    public void addFirst(T obj) {
+
+    }
+
+    public void addLast(T obj) {
+
+    }
+
+    public void addWithIndex(int i, T obj) {
+
+    }
+
+
+    public T getFirst() {
+
+    }
+
+    public T getLast() {
+
+    }
+
+    public void getWithIndex(int i) {
+
+    }
+
+
+    public void removeFirst() {
+
+    }
+
+    public void removeLast() {
+
+    }
+
+    public void removeWithIndex(int index) {
+
+    }
+
+    public int size() {
+        return size;
+    }
+
+
+    private static class Node<E> {
+        Node<E> prev;
+        E data;
+        Node<E> next;
+
+        public Node(Node<E> prev, E data, Node<E> next) {
+            this.prev = prev;
+            this.data = data;
+            this.next = next;
+        }
+    }
+}
