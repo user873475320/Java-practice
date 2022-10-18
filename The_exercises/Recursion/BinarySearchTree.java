884b64b35a94b3baea1c965a3efa5861f1ea98bd
none <user873475320@gmail.com>
2022-10-18 18:22:44 +0500

    2022-10-18

--- The_exercises/Recursion/BinarySearchTree.java
+++ The_exercises/Recursion/BinarySearchTree.java
@@ -1,7 +1,22 @@
 package The_exercises.Recursion;
 
-public class BinarySearchTree {
-    class Node {
+class Tree {
+    private Node root;
+
+    public double find(int key) {
+
+    }
+
+    public void insert(int id, double data) {
+
+    }
+
+    public void delete(int id) {
+
+    }
+
+
+    static class Node {
         public int key;
         public double data;
         public Node leftChild;
@@ -9,6 +24,23 @@ public class BinarySearchTree {
 
         public void printNode() {
             System.out.println("{" + key + ", " + data + "}");
+        }
+    }
+}
+
+class TreeDemo {
+    public static void main(String[] args) {
+        Tree tree = new Tree();
+
+        tree.insert(0, 1.2);
+        tree.insert(1, 2.2);
+        tree.insert(4, 6.8);
+
+        double found = tree.find(4);
+        if (found == 0) {
+            System.out.println("Not found");
+        } else {
+            System.out.println("Found ");
         }
     }
 }
